870a2854456ecb25b906c3c0508dd392cdfdbd01
Dakshesh <Dakshesh@LAPTOP-JHHLMP8T>
2025-03-23 00:54:32 +0530

    This is my first commit

---
+++ Java_Programs/src/FindEvenOdd.java
@@ -0,0 +1,27 @@
+
+public class FindEvenOdd {
+
+	public static void main(String[] args) {
+		
+		FindEvenOdd FED= new FindEvenOdd();
+		FED.FindEvenOddNumber ();
+
+	}
+
+	public void FindEvenOddNumber() {
+	int a[] = {1,2,3,4,5,6,7,8,9,10,11};
+	
+	for(int i=1;i<=a.length-1;i++)
+	{
+		if (a[i]%2==0)
+		{
+			System.out.println(a[i] + " "+ "is even number");
+		}
+		
+		else
+			System.out.println(a[i] + " "+ "is even odd");
+	}
+	
+	}
+
+}
